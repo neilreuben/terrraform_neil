2ed32bced5e30ee5b33f78f6b76d80f4e666d8c0
Neil Reuben <neil.reuben@autodesk.com>
2019-09-16 10:55:57 -0700

    added another subnet with security group

--- resoources.tf
+++ resoources.tf
@@ -17,3 +17,21 @@ resource "aws_subnet" "subnet1" {
 	vpc_id = "${aws_vpc.environment-example-two.id}"
 	availability_zone = "us-west-2a"
 }
+resource "aws_subnet" "subnet2" {
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 2, 2)}"
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+  availability_zone = "us-west-2b"
+}
+
+resource "aws_secity_group" "subnetsecurity" {
+	vpc_id = "${aws_vpc.environment-example-two.id}"
+	ingress	{
+		cider_blocks = [
+			"${aws_vpc.environment-example-two.cidr_block}"
+		]
+		from_port = 80
+		to_port = 80
+		protocol = "TCP"
+	}
+  
+}
